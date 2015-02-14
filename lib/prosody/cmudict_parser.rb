7380a23b41547303474e8a77d1971ee113bfc819
kjhenner <kjhenner@gmail.com>
2015-02-13 19:34:13 -0800

    parse phones and stress

--- lib/prosody/cmudict_parser.rb
+++ lib/prosody/cmudict_parser.rb
@@ -2,11 +2,17 @@ module Prosody::CMUDictParser
   require 'json'
   def parse_cmudict
     cmudict = File.open('data/cmudict', 'r'){ |f| f.read }
-    matches = cmudict.scan(/^(?<word>\w+)\(?(?<alternate>\d+)?\)?\s+(?<phones>[\w\d ]+)$/)
-    matches = cmudict.scan(/^(?<word>[\w\d]+)\s+(?<phones>[\w\d ]+)$/)
-    dict = {}
-    matches.each{ |e| dict[e[0]] = dict[e[0]] ? dict[e[0]].concat(e[1].split) : [e[1].split]}
-    return dict
+    matches = cmudict.scan(/^([\w\d]+)\s+([\w\d ]+)$/)
+    matches.inject({}) do |memo, e|
+      memo[e[0]] = memo[e[0]] ? memo[e[0]].concat(parse_phonemes(e[1])) : [parse_phonemes(e[1])]}
+      memo
+    end
+  end
+  def parse_phonemes(phoneme_string)
+    phonemes = phoneme_string.split.collect do |p|
+      m = /(\w+)(\d)?/.match(p)
+      {phoneme: m[0], :stress: m[1]}
+    end
   end
   def serialize_cmudict(dict)
     File.open("data/cmudict.json", "w") do |f|
