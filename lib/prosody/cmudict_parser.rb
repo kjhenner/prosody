d9e9a66f9ccb1a119ced93d2028d57fe2d6ac0b4
kjhenner <kjhenner@gmail.com>
2015-02-14 11:13:45 -0800

    phonemes and stresses for cmudict

--- lib/prosody/cmudict_parser.rb
+++ lib/prosody/cmudict_parser.rb
@@ -1,27 +1,29 @@
-module Prosody::CMUDictParser
-  require 'json'
-  def parse_cmudict
-    cmudict = File.open('data/cmudict', 'r'){ |f| f.read }
-    matches = cmudict.scan(/^([\w\d]+)\s+([\w\d ]+)$/)
-    matches.inject({}) do |memo, e|
-      memo[e[0]] = memo[e[0]] ? memo[e[0]].concat(parse_phonemes(e[1])) : [parse_phonemes(e[1])]}
-      memo
+module Prosody
+  module CMUDictParser
+    require 'json'
+    def parse_cmudict
+      cmudict = File.open('data/cmudict', 'r'){ |f| f.read }
+      matches = cmudict.scan(/^([\w\d]+)\s+([\w\d ]+)$/)
+      matches.inject({}) do |memo, e|
+        memo[e[0]] = memo[e[0]] ? memo[e[0]].concat(parse_phonemes(e[1])) : [parse_phonemes(e[1])]
+        memo
+      end
     end
-  end
-  def parse_phonemes(phoneme_string)
-    phonemes = phoneme_string.split.collect do |p|
-      m = /(\w+)(\d)?/.match(p)
-      {phoneme: m[0], :stress: m[1]}
+    def parse_phonemes(phoneme_string)
+      phonemes = phoneme_string.split.collect do |p|
+        m = /(\D+)(\d)?/.match(p)
+        {phoneme: m[1], stress: m[2]}
+      end
     end
-  end
-  def serialize_cmudict(dict)
-    File.open("data/cmudict.json", "w") do |f|
-      f.write(dict.to_json)
+    def serialize_cmudict(dict)
+      File.open("data/cmudict.json", "w") do |f|
+        f.write(dict.to_json)
+      end
     end
-  end
-  def load_cmudict_from_json
-    File.open("../data/cmudict.json", "r") do |f|
-      JSON.parse(File.read(f))
+    def load_cmudict_from_json
+      File.open("../data/cmudict.json", "r") do |f|
+        JSON.parse(File.read(f))
+      end
     end
   end
 end
